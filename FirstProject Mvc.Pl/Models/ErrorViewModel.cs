0c080667f5a3963d2933b37eae85e782f025be09
ahmedf777686 <142512678+ahmedf777686@users.noreply.github.com>
2024-03-20 05:36:38 +0200

    Add project files.

---
+++ FirstProject Mvc.Pl/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace FirstProject_Mvc.Pl.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
