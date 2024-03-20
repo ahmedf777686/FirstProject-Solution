b0d0604a628d9f7f22dbea87385c2a6a3dea0a5a
ahmedf777686 <142512678+ahmedf777686@users.noreply.github.com>
2024-03-21 01:15:04 +0200

    Part 07 Generate Migration

---
+++ FirstProject Mvc.DAL/Migrations/20240320231335_initialcreate.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace FirstProject_Mvc.DAL.Migrations
+{
+    public partial class initialcreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "departments",
+                columns: table => new
+                {
+                    ID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "10, 10"),
+                    Name = table.Column<string>(type: "varchar", nullable: false),
+                    Code = table.Column<string>(type: "varchar", nullable: false),
+                    DateOfCreation = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_departments", x => x.ID);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "departments");
+        }
+    }
+}
