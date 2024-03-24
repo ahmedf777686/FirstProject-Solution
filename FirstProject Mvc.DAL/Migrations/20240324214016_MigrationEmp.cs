2ef97af31a07ad2ceec47114e09516b3941eb956
ahmedf777686 <142512678+ahmedf777686@users.noreply.github.com>
2024-03-24 23:42:47 +0200

    Part 05 Employee - DAL

---
+++ FirstProject Mvc.DAL/Migrations/20240324214016_MigrationEmp.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace FirstProject_Mvc.DAL.Migrations
+{
+    public partial class MigrationEmp : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Employee",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    Age = table.Column<int>(type: "int", nullable: false),
+                    Address = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Salary = table.Column<decimal>(type: "decimal(12,2)", nullable: false),
+                    IsActive = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Email = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    PhoneNumber = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    HiringDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    CreationDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Gender = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    EmpType = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Employee", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Employee");
+        }
+    }
+}
---
+++ FirstProject Mvc.DAL/Models/Employee.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstProject_Mvc.DAL.Models
+{
+	public enum Gender
+	{
+		[EnumMember(Value = "Male")]
+
+		Male = 1,
+		[EnumMember(Value = "Female")]
+		Female
+	}
+
+	public enum EmpType
+	{
+		FullTime = 1,
+		PartTime
+	}
+
+	public class Employee
+	{
+
+		public int Id { get; set; }
+		[Required]
+		[MaxLength(50, ErrorMessage = "Name is Required !")]
+		[MinLength(5, ErrorMessage = "Min length of Name  is 5 chars")]
+
+		public string Name { get; set; }
+		[Range(22, 34)]
+		public int Age { get; set; }
+
+		public string Address { get; set; }
+
+		[DataType(DataType.Currency)]
+		public decimal Salary { get; set; }
+		[Display(Name = "Is Active")]
+		public string IsActive { get; set; }
+		[EmailAddress]
+
+		public string Email { get; set; }
+		[Display(Name = "Phone Number")]
+		[Phone]
+		public string PhoneNumber { get; set; }
+		[Display(Name = "Hiring Date")]
+
+		public DateTime HiringDate { get; set; }
+
+		public DateTime CreationDate { get; set; }
+
+		public Gender Gender { get; set; }
+
+		public EmpType EmpType { get; set; }
+	}
+}
